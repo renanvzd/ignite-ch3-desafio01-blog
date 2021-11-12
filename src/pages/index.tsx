06319aca4fb469f122618f59f37e8f09823f63e9
Renan Veronez Drechsler <renanvz@outlook.com>
2021-11-12 13:29:21 -0300

    Home - Load more posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import { GetStaticProps } from 'next';
 import Head from 'next/head';
+import Prismic from '@prismicio/client';
+import { useState } from 'react';
+import { GetStaticProps } from 'next';
 import { AiOutlineSchedule } from 'react-icons/ai';
 import { FiUser } from 'react-icons/fi';
-import Header from '../components/Header';
-
+import Link from 'next/link';
 import { getPrismicClient } from '../services/prismic';
-
-import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+import { formatDate } from '../utils';
 
 interface Post {
   uid?: string;
@@ -28,7 +28,32 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-export default function Home(): JSX.Element {
+export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const [nextPage, setNextPage] = useState<string>(postsPagination.next_page);
+
+  const formattedPosts = postsPagination.results.map(post => {
+    return {
+      ...post,
+      first_publication_date: formatDate(post.first_publication_date),
+    };
+  });
+
+  const [posts, setPosts] = useState<Post[]>(formattedPosts);
+
+  async function handleLoadMorePosts(): Promise<void> {
+    const newPosts = await fetch(nextPage).then(response => response.json());
+
+    setNextPage(newPosts.next_page);
+
+    const newFormattedPosts = newPosts.results.map(post => {
+      return {
+        ...post,
+        first_publication_date: formatDate(post.first_publication_date),
+      };
+    });
+
+    setPosts([...posts, ...newFormattedPosts]);
+  }
   return (
     <>
       <Head>
@@ -36,51 +61,71 @@ export default function Home(): JSX.Element {
       </Head>
       <main className={styles.contentContainer}>
         <section className={styles.hero}>
-          <h1>Como utilizar hooks</h1>
-          <p>Pensando em sincronização em vez de ciclos de vida.</p>
-          <div className={styles.subsection}>
-            <div className={styles.schedule}>
-              <AiOutlineSchedule color="#BBBBBB" />
-              <span>15 Mar 2021</span>
-            </div>
+          {posts?.map(post => (
+            <div className={styles.containerPost}>
+              <Link key={post.uid} href={`/post/${post.uid}`}>
+                <a>
+                  <div className={styles.titlePost}>{post.data.title}</div>
+                  <p>{post.data.subtitle}</p>
+                  <div className={styles.subsection}>
+                    <div className={styles.schedule}>
+                      <AiOutlineSchedule color="#BBBBBB" />
+                      <span>{post.first_publication_date}</span>
+                    </div>
 
-            <div className={styles.author}>
-              <FiUser color="#BBBBBB" />
-              <span>Renan Veronez Drechsler</span>
+                    <div className={styles.author}>
+                      <FiUser color="#BBBBBB" />
+                      <span>{post.data.author}</span>
+                    </div>
+                  </div>
+                </a>
+              </Link>
             </div>
-          </div>
+          ))}
+          {nextPage && (
+            <button type="button" onClick={handleLoadMorePosts}>
+              <div className={styles.loadMorePosts}>
+                <p>Carregar mais posts...</p>
+              </div>
+            </button>
+          )}
         </section>
-
-        <section className={styles.hero}>
-          <h1>Criando um app CRA do zero</h1>
-          <p>
-            Tudo sobre como criar a sua primeira aplicação utilizando Create
-            React App
-          </p>
-          <div className={styles.subsection}>
-            <div className={styles.schedule}>
-              <AiOutlineSchedule color="#BBBBBB" />
-              <span>19 Abr 2021</span>
-            </div>
-
-            <div className={styles.author}>
-              <FiUser color="#BBBBBB" />
-              <span>Renan Veronez</span>
-            </div>
-          </div>
-        </section>
-
-        <div className={styles.loadMorePosts}>
-          <p>Carregar mais posts</p>
-        </div>
       </main>
     </>
   );
 }
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
 
-//   // TODO
-// };
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+      pageSize: 1,
+    }
+  );
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      first_publication_date: post.first_publication_date,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+    };
+  });
+
+  const postsPagination = {
+    next_page: postsResponse.next_page,
+    results: posts,
+  };
+
+  return {
+    props: {
+      postsPagination,
+    },
+  };
+};
